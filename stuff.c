5b73802d91ea30f10e055408aadf5a12f3d72020
gongas123lol <114987902+gongas123lol@users.noreply.github.com>
2023-12-25 22:38:10 +0000

    Update stuff.c

--- stuff.c
+++ stuff.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <glib.h>
+
 
 void dictionary_add(Dictionary *dictionary, const char *filename) {
 	
@@ -31,3 +34,46 @@ int sumofASCII(const char *line) {
 
     return sum;
 }
+//----------------------------------------------------------------
+typedef struct Dictionary {
+    GHashTable *hashTable;
+} Dictionary;
+
+Dictionary *dictionary_create() {
+    Dictionary *dictionary = (Dictionary *)malloc(sizeof(Dictionary));
+    if (dictionary != NULL) {
+        dictionary->hashTable = g_hash_table_new(g_str_hash, g_str_equal);
+    }
+    return dictionary;
+}
+
+// Add words from a file to the dictionary
+void dictionary_add(Dictionary *dictionary, const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return;
+    }
+
+    char word[100]; //100 caracteres deve ser suficiente
+	
+
+    while (fscanf(file, "%s", word) == 1) {
+        // Use the word as both the key and the value in the hashtable
+        g_hash_table_insert(dictionary->hashTable, strdup(word), strdup(word));
+    }
+
+    fclose(file);
+}
+
+int dictionary_lookup(Dictionary *dictionary, const char *word) {
+    return g_hash_table_contains(dictionary->hashTable, word);
+}
+
+// Destroy the dictionary and free associated resources
+void dictionary_destroy(Dictionary *dictionary) {
+    g_hash_table_destroy(dictionary->hashTable);
+    free(dictionary);
+}
+
+
